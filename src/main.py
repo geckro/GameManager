eaf339b2c8ead7e3048f38035d1715151997037f
geck <94733731+geckro@users.noreply.github.com>
2024-06-02 11:58:38 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 # LICENSE: MIT
-import argparse
 import sys
-import src
 
 from PyQt6.QtWidgets import QApplication
 
@@ -9,24 +7,11 @@ from core.log import log
 from ui.app import MainWindow
 
 
-def argument_parser():
-    """
-    Parse arguments using the python standard library argparse.
-    """
-    parser = argparse.ArgumentParser(prog='gamemanager', description=src.description)
-    parser.add_argument('-v', '--version', help='Show app version')
-    return parser
-
-
 def main():
     """
     Start the application.
     """
     try:
-        parser = argument_parser()
-        argv = sys.argv[1:]
-        arguments = parser.parse_args(argv)
-
         gui_app = QApplication([])
         window = MainWindow()
         window.show()
